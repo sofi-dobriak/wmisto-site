c6ebd137409c3a56100d9d2a2a68d27aaea80563
Sofi Dobriak <sofi.dobriak@gmail.com>
2026-03-13 10:17:18 +0200

    home page: add w-diagonal section

---
+++ src/pages/home/w-diagonal-section/w-diagonal-section.js
@@ -0,0 +1,18 @@
+import "./w-diagonal-section.scss";
+
+import Swiper from "swiper";
+import { Navigation, Parallax } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/effect-fade";
+
+const swiper = new Swiper(".w-diagonal-swiper", {
+  modules: [Navigation, Parallax],
+  speed: 1000,
+  slidesPerView: 1,
+  loop: true,
+  parallax: true,
+  navigation: {
+    prevEl: ".w-diagonal-button-prev",
+    nextEl: ".w-diagonal-button-next",
+  },
+});
