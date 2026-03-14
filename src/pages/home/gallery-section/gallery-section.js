d5024d3e83034e603ffe691daf41bdcac38b1524
Sofi Dobriak <sofi.dobriak@gmail.com>
2026-03-14 17:05:16 +0200

    home page: add gallery section

--- src/pages/home/gallery-section/gallery-section.js
+++ src/pages/home/gallery-section/gallery-section.js
@@ -1,1 +1,19 @@
 import "./gallery-section.scss";
+import "../../../shared/components/swiper-nav/swiper-nav.scss";
+
+import Swiper from "swiper";
+import { Navigation } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/effect-fade";
+
+const swiper = new Swiper(".gallery-swiper", {
+  modules: [Navigation],
+  speed: 1000,
+  slidesPerView: 3,
+  spaceBetween: 20,
+  navigation: {
+    prevEl: ".gallery-button-prev",
+    nextEl: ".gallery-button-next",
+  },
+  loop: true,
+});
