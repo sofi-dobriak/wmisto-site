d924a8365125bbc79d59a7e91346b11a91e73f99
Sofi Dobriak <sofi.dobriak@gmail.com>
2026-03-18 15:42:53 +0200

    adaptive: genplan and w-diagonal sections

--- src/pages/home/w-diagonal-section/w-diagonal-section.js
+++ src/pages/home/w-diagonal-section/w-diagonal-section.js
@@ -10,7 +10,7 @@ gsap.registerPlugin(ScrollTrigger);
 
 document.addEventListener("DOMContentLoaded", () => {
   initDiagonalSwiper();
-  initDiagonalAnim();
+  // initDiagonalAnim();
 });
 
 function initDiagonalSwiper() {
